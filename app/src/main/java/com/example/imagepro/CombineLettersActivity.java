6c1a32c9cb351c1f4ececea7310c25bdb6c15820
DESKTOP-FNF81BL\Dell <bismeetchawla.2000@gmail.com>
2022-05-16 23:09:19 +0530

    Bluetooth Integrated

--- app/src/main/java/com/example/imagepro/CombineLettersActivity.java
+++ app/src/main/java/com/example/imagepro/CombineLettersActivity.java
@@ -6,16 +6,32 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -26,7 +42,12 @@ import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
 import java.util.Locale;
+import java.util.Set;
+import java.util.UUID;
 
 public class CombineLettersActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2{
     private static final String TAG="MainActivity";
@@ -38,7 +59,192 @@ public class CombineLettersActivity extends Activity implements CameraBridgeView
     private Button add_button;
     private Button clear_button;
     private Button play_button;
+    private Button send_button;
     private TextView change_text;
+//    public DrawerLayout drawerLayout;
+//    public ActionBarDrawerToggle actionBarDrawerToggle;
+    private LinearLayout mGallery;
+    private int[] mImgIds;
+    private LayoutInflater mInflater;
+
+    private static final UUID MY_UUID_INSECURE =
+            UUID.fromString("8ce255c0-200a-11e0-ac64-0800200c9a66");
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    private BluetoothDevice mmDevice;
+    private UUID deviceUUID;
+    ConnectedThread mConnectedThread;
+    private Handler handler;
+
+//    String TAG = "MainActivity";
+    EditText send_data;
+    TextView view_data;
+    StringBuilder messages;
+
+    public void pairDevice(View v) {
+        toastMsg("Pair device started");
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        Log.e("MAinActivity", "" + pairedDevices.size());
+        if (pairedDevices.size() > 0) {
+            Object[] devices = pairedDevices.toArray();
+            BluetoothDevice device = (BluetoothDevice) devices[0];
+            //ParcelUuid[] uuid = device.getUuids();
+            Log.e("MAinActivity", "" + device);
+            toastMsg("The device is" + device);
+            //Log.e("MAinActivity", "" + uuid)
+
+            ConnectThread connect = new ConnectThread(device, MY_UUID_INSECURE);
+            connect.start();
+
+        }
+    }
+
+    private class ConnectThread extends Thread {
+        private BluetoothSocket mmSocket;
+
+        public ConnectThread(BluetoothDevice device, UUID uuid) {
+            Log.d(TAG, "ConnectThread: started.");
+            mmDevice = device;
+            deviceUUID = uuid;
+        }
+
+        public void run() {
+            BluetoothSocket tmp = null;
+            Log.i(TAG, "RUN mConnectThread ");
+
+            // Get a BluetoothSocket for a connection with the
+            // given BluetoothDevice
+            try {
+                Log.d(TAG, "ConnectThread: Trying to create InsecureRfcommSocket using UUID: "
+                        + MY_UUID_INSECURE);
+                tmp = mmDevice.createRfcommSocketToServiceRecord(MY_UUID_INSECURE);
+            } catch (IOException e) {
+                Log.e(TAG, "ConnectThread: Could not create InsecureRfcommSocket " + e.getMessage());
+            }
+
+            mmSocket = tmp;
+
+            // Make a connection to the BluetoothSocket
+
+            try {
+                // This is a blocking call and will only return on a
+                // successful connection or an exception
+                mmSocket.connect();
+
+            } catch (IOException e) {
+                // Close the socket
+                try {
+                    mmSocket.close();
+                    Log.d(TAG, "run: Closed Socket.");
+                } catch (IOException e1) {
+                    Log.e(TAG, "mConnectThread: run: Unable to close connection in socket " + e1.getMessage());
+                }
+                Log.d(TAG, "run: ConnectThread: Could not connect to UUID: " + MY_UUID_INSECURE);
+            }
+
+            //will talk about this in the 3rd video
+            connected(mmSocket);
+        }
+
+        public void cancel() {
+            try {
+                Log.d(TAG, "cancel: Closing Client Socket.");
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "cancel: close() of mmSocket in Connectthread failed. " + e.getMessage());
+            }
+        }
+    }
+
+    private void connected(BluetoothSocket mmSocket) {
+        Log.d(TAG, "connected: Starting.");
+
+        // Start the thread to manage the connection and perform transmissions
+        mConnectedThread = new ConnectedThread(mmSocket);
+        mConnectedThread.start();
+    }
+
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            Log.d(TAG, "ConnectedThread: Starting.");
+
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+
+            try {
+                tmpIn = mmSocket.getInputStream();
+                tmpOut = mmSocket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+
+            int bytes; // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                // Read from the InputStream
+                try {
+                    bytes = mmInStream.read(buffer);
+                    final String incomingMessage = new String(buffer, 0, bytes);
+                    Log.d(TAG, "InputStream: " + incomingMessage);
+
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            change_text.setText(incomingMessage);
+                        }
+                    });
+
+
+                } catch (IOException e) {
+                    Log.e(TAG, "write: Error reading Input Stream. " + e.getMessage());
+                    break;
+                }
+            }
+        }
+
+
+        public void write(byte[] bytes) {
+            String text = new String(bytes, Charset.defaultCharset());
+            Log.d(TAG, "write: Writing to outputstream: " + text);
+            toastMsg("write: Writing to outputstream: " + text);
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) {
+                Log.e(TAG, "write: Error writing to output stream. " + e.getMessage());
+            }
+        }
+
+        /* Call this from the main activity to shutdown the connection */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    public void SendMessage(View v) {
+        toastMsg("sending message now");
+        byte[] bytes = change_text.getText().toString().getBytes(Charset.defaultCharset());
+        mConnectedThread.write(bytes);
+    }
+
 
     private BaseLoaderCallback mLoaderCallback =new BaseLoaderCallback(this) {
         @Override
@@ -78,7 +284,16 @@ public class CombineLettersActivity extends Activity implements CameraBridgeView
             ActivityCompat.requestPermissions(CombineLettersActivity.this, new String[] {Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
         }
 
+//        setContentView(R.layout.nav_activity_combineletters);
         setContentView(R.layout.activity_combine_letters);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        setContentView(R.layout.activity_main);
+//        mInflater = LayoutInflater.from(this);
+//        initData();
+//        initView();
+//        LayoutInflater inflater = LayoutInflater.from(this);
+//        View myView = inflater.inflate(R.layout.activity_combine_letters, null, false);
+//        drawer.addView(myView, 1);
 
         mOpenCvCameraView=(CameraBridgeViewBase) findViewById(R.id.frame_Surface);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
@@ -88,6 +303,29 @@ public class CombineLettersActivity extends Activity implements CameraBridgeView
         add_button = findViewById(R.id.add_button);
         change_text = findViewById(R.id.change_text);
         play_button = findViewById(R.id.play_button);
+        send_button = findViewById(R.id.send_button);
+
+//        send_data = (EditText) findViewById(R.id.editText);
+//        view_data = (TextView) findViewById(R.id.textView);
+
+        if (bluetoothAdapter != null && !bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new
+                    Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        // drawer layout instance to toggle the menu icon to open
+        // drawer and back button to close drawer
+//        drawerLayout = findViewById(R.id.my_drawer_layout);
+//        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+//
+//        // pass the Open and Close toggle for the drawer layout listener
+//        // to toggle the button
+//        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+//        actionBarDrawerToggle.syncState();
+//
+//        // to make the Navigation drawer icon always appear on the action bar
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
@@ -110,6 +348,14 @@ public class CombineLettersActivity extends Activity implements CameraBridgeView
             }
         });
 
+//        send_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                byte[] bytes = change_text.getText().toString().getBytes(Charset.defaultCharset());
+//                mConnectedThread.write(bytes);
+//            }
+//        });
+
         try{
             signLanguageClass=new signLanguageClass(clear_button, add_button, change_text, getAssets(),"hand_model.tflite","custom_label.txt",300, "Sign_language_model.tflite", 96);
             Log.d("MainActivity","Model is successfully loaded");
@@ -118,6 +364,79 @@ public class CombineLettersActivity extends Activity implements CameraBridgeView
             Log.d("MainActivity","Getting some error");
             e.printStackTrace();
         }
+    }
+
+    public void toastMsg(String msg) {
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+//    public void displayToastMsg(View v) {
+//        toastMsg("Hello how are you today!!");
+//    }
+
+    public void Start_Server(View view) {
+        toastMsg("Starting server now");
+        AcceptThread accept = new AcceptThread();
+        accept.start();
+
+    }
+
+    private class AcceptThread extends Thread {
+
+        // The local server socket
+        private final BluetoothServerSocket mmServerSocket;
+
+        public AcceptThread() {
+            BluetoothServerSocket tmp = null;
+
+            // Create a new listening server socket
+            try {
+                tmp = bluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord("appname", MY_UUID_INSECURE);
+
+                Log.d(TAG, "AcceptThread: Setting up Server using: " + MY_UUID_INSECURE);
+            } catch (IOException e) {
+                Log.e(TAG, "AcceptThread: IOException: " + e.getMessage());
+            }
+
+            mmServerSocket = tmp;
+        }
+
+        public void run() {
+            Log.d(TAG, "run: AcceptThread Running.");
+
+            BluetoothSocket socket = null;
+
+            try {
+                // This is a blocking call and will only return on a
+                // successful connection or an exception
+                Log.d(TAG, "run: RFCOM server socket start.....");
+
+                socket = mmServerSocket.accept();
+
+                Log.d(TAG, "run: RFCOM server socket accepted connection.");
+
+            } catch (IOException e) {
+                Log.e(TAG, "AcceptThread: IOException: " + e.getMessage());
+            }
+
+            //talk about this is in the 3rd
+            if (socket != null) {
+                connected(socket);
+            }
+
+            Log.i(TAG, "END mAcceptThread ");
+        }
+
+        public void cancel() {
+            Log.d(TAG, "cancel: Canceling AcceptThread.");
+            try {
+                mmServerSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "cancel: Close of AcceptThread ServerSocket failed. " + e.getMessage());
+            }
+        }
+
     }
 
     @Override
@@ -167,5 +486,19 @@ public class CombineLettersActivity extends Activity implements CameraBridgeView
 
         return out;
     }
+
+    // override the onOptionsItemSelected()
+    // function to implement
+    // the item click listener callback
+    // to open and close the navigation
+    // drawer when the icon is clicked
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//
+//        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
 }
